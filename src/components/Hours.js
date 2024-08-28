9c35640938903aa505b792d4924fb4ace15fead9
Luis Torres <42155580+luisdtorresp@users.noreply.github.com>
2024-08-28 19:29:54 +0000

    Agrega Horarios Reto 2

--- src/components/Hours.js
+++ src/components/Hours.js
@@ -1,1 +1,31 @@
-// Placeholder to host a component
+// PComponente React que muestra las horas de apertura y cierre de un local. Muestra las horas en las que el refugio está abierto durante el día actual. El refugio abre de 10 a.m. a 4 p.m. de lunes a viernes, y de 9 a.m. a 8 p.m. los sábados y domingos.
+
+import React from 'react';  
+
+
+
+const Hours = () => {
+
+    const hours = {
+        0: 'Domingos: 9 a.m. - 8 p.m.',
+        1: 'Lunes: 10 a.m. - 4 p.m.',
+        2: 'Martes: 10 a.m. - 4 p.m.',
+        3: 'Miercoles: 10 a.m. - 4 p.m.',
+        4: 'Jueves: 10 a.m. - 4 p.m.',
+        5: 'Viernes: 10 a.m. - 4 p.m.',
+        6: 'Sabados: 9 a.m. - 8 p.m.'
+    };
+        
+
+  const today = new Date().getDay();
+  const todayHours = hours[today];
+  
+  return (
+    <div>
+      <h2>Today's Hours:</h2>
+      <p>{todayHours}</p>
+    </div>
+  );
+}
+
+export default Hours;
